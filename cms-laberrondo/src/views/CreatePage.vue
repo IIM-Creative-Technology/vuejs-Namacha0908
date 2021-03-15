7a7d589f8c0f12d7faeeb49b93f31b428adb7863
Tlaberrondo <theo.laberrondo@edu.devinci.fr>
2021-03-15 18:50:22 +0100

    build<vuex>: ajout de vuex :wrench:

--- cms-laberrondo/src/views/CreatePage.vue
+++ cms-laberrondo/src/views/CreatePage.vue
@@ -6,10 +6,10 @@
         <label for="meta">Meta Title :</label>
         <input name="meta" type="text" required>
         <label for="img">Ajouter une image</label>
-        <input name="img" type="image" required>
+        <input name="img" type="file" accept="image/png, image/jpeg" required>
         <label for="post">Ajouter une image</label>
         <textarea name="post" required></textarea>
-        <input type=submit value="Créer la page">
+        <input type="submit" value="Créer la page">
   </section>
 </template>
 
